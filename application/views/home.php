c2ba6d02e00c9a8b1feb1cdec0c021e3bc2fed11
Andy <andy@airslash.net>
2015-08-29 00:32:58 +0700

    updates food and workout data + some notes at homepage

--- application/views/home.php
+++ application/views/home.php
@@ -1,8 +1,13 @@
 
     <div class="row">
       <div class="twelve columns" style="margin-top: 5%;">
-        <h4>Situs Andy</h4>
+        <h4>Andy's Homepage</h4>
         <p>☃</p>
+        <h5>What's new?</h5>
+        <ul>
+            <li>08/29/15: Day 29 of <strong>August's Plank-a-Day</strong>. None missed so far. Time flies so fast.</li>
+            <li>08/09/15: Day 9 of <strong>August's Plank-a-Day</strong>. Doing OK.</li>
+        </ul>
       </div>
     </div>
 
